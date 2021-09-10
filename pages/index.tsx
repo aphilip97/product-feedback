13876579c138f5d82550605c11324c3f086003b5
Athul Philip <athulphilip1309@gmail.com>
2021-09-10 15:05:07 +0100

    :tada: Initial commit.

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <header>
+        <h1>Product Feedback App</h1>
+      </header>
+    </>
+  );
+};
+
+export default Home;
