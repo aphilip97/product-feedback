96107ef636457f970700fb76cc64409dc84502c0
Athul Philip <athulphilip1309@gmail.com>
2021-09-22 21:23:30 +0100

    :sparkles: Update hamburger menu button component.

--- src/components/Hamburger/index.tsx
+++ src/components/Hamburger/index.tsx
@@ -1,45 +1,17 @@
-import { Dispatch, SetStateAction, useState } from 'react';
+import type { HamburgerProps } from '../../../types';
 import styles from './index.module.sass';
 
-type HamburgerProps = {
-  open: boolean;
-  clickHandler: Dispatch<SetStateAction<boolean>>;
-};
-
 const Hamburger: React.FC<HamburgerProps> = ({
-  open,
-  clickHandler: setOpen
+  menuState,
+  menuAnimStage,
+  clickHandler,
 }) => {
-
-  type AnimationStage = 'start' | 'middle' | 'end';
-
-  const [animStage, setAnimStage] = useState<AnimationStage>('start');
-
-  const getAnimationStageOrder = (): [
-    AnimationStage,
-    AnimationStage,
-    AnimationStage
-  ] => {
-    if (!open) return ['start', 'middle', 'end'];
-    return ['end', 'middle', 'start'];
-  };
-
-  const clickHandler = () => {
-    const order = getAnimationStageOrder();
-    setAnimStage(order[0]);
-    setTimeout(() => {
-      setAnimStage(order[1]);
-      setTimeout(() => {
-        setAnimStage(order[2]);
-        setOpen(!open);
-      }, 300);
-    }, 150);
-  };
 
   return (
     <button
-      data-open={open}
-      data-animation-stage={animStage}
+      data-open={menuState}
+      data-animation-stage={menuAnimStage}
+      disabled={menuAnimStage === 'middle'}
       className={styles['hamburger']}
       onClick={clickHandler}
     >
