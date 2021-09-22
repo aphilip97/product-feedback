ff9e88dfa75444697ddd46166ebb20ebb06b5e3a
Athul Philip <athulphilip1309@gmail.com>
2021-09-22 21:22:40 +0100

    :sparkles: Implement CategoryFilter component.

---
+++ src/components/CategoryFilter/index.tsx
@@ -0,0 +1,52 @@
+import type { Dispatch, SetStateAction } from 'react';
+import CategoryTag from '../CategoryTag';
+import styles from './index.module.sass';
+
+type Category = {
+  content: string;
+  acronym?: true;
+  expansion?: string;
+}
+
+type CategoryFilter = {
+  activeFilter: number;
+  changeFilter: Dispatch<SetStateAction<number>>;
+}
+
+const CategoryFilter: React.FC<CategoryFilter> = ({
+  activeFilter,
+  changeFilter
+}) => {
+
+  const hardcodedCategories: Category[] = [
+    { content: "All" },
+    { content: "UI", acronym: true, expansion: 'User Interface' },
+    { content: "UX", acronym: true, expansion: 'User Experience' },
+    { content: "Enhancement" },
+    { content: "Bug" },
+    { content: "Feature" },
+  ];
+
+  const createClickHandler = (index: number) => () => {
+    changeFilter(index);
+  };
+
+  return (
+    <div role='presentation' className={styles['container']}>
+      {hardcodedCategories.map((category, index) => {
+        return (
+          <CategoryTag
+            key={index}
+            active={index === activeFilter}
+            clickHandler={createClickHandler(index)}
+          >
+            {category.acronym ? <abbr title={category.expansion}>{category.content}</abbr> : category.content}
+          </CategoryTag>
+        );
+      })}
+    </div>
+  );
+
+};
+
+export default CategoryFilter;
