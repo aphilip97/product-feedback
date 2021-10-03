c3371b85d5b8c8818e7fd9f7d9c50aff42d4c3fc
Athul Philip <athulphilip1309@gmail.com>
2021-10-03 19:29:14 +0100

    :sparkles: Implement CustomTextArea component.

---
+++ src/components/CustomTextArea/index.tsx
@@ -0,0 +1,75 @@
+import type {
+  ChangeEventHandler,
+  Dispatch,
+  SetStateAction,
+  TextareaHTMLAttributes,
+} from 'react';
+
+import { useState } from 'react';
+import styles from './index.module.sass';
+
+type CustomTextAreaProps = {
+
+  ident: string;
+
+  value: TextareaHTMLAttributes<
+    HTMLTextAreaElement
+  >['value'];
+
+  setValue: Dispatch<
+    SetStateAction<
+      string
+    >
+  >;
+
+  placeholder?: TextareaHTMLAttributes<
+    HTMLTextAreaElement
+  >['placeholder'];
+
+};
+
+const CustomTextArea: React.FC<
+  CustomTextAreaProps
+> = ({
+  ident,
+  value,
+  setValue,
+  placeholder,
+  children,
+}) => {
+
+  const [invalid, setInvalid] = useState(false);
+
+  const handleChange: ChangeEventHandler<
+    HTMLTextAreaElement
+  > = (evt) => {
+    const { currentTarget: { value } } = evt;
+    const val = value.trim();
+    val.length === 0 ? setInvalid(true) : setInvalid(false);
+    setValue(value);
+  };
+
+  return (
+    <fieldset className={styles['custom-text-area']}>
+      {children}
+      <textarea
+        id={`${ident}Input`}
+        name={`${ident}`}
+        aria-describedby={`${ident}-help`}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        minLength={1}
+        maxLength={250}
+        onInvalid={() => setInvalid(true)}
+        data-invalid={invalid}
+      />
+      <span>
+        <small className={styles['error']} role="alert">Can't be empty</small>
+      </span>
+    </fieldset>
+  );
+};
+
+export default CustomTextArea;
